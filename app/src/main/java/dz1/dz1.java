30582d036d4ed349c78d30dc0ed094165529cd8a
mixonko <m1zan1rop@mail.ru>
2019-02-27 18:17:18 +0300

    Java конвеншн

--- app/src/main/java/dz1/dz1.java
+++ app/src/main/java/dz1/dz1.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import test.com.homework.R;
 
 public class dz1 extends Activity implements View.OnClickListener {
