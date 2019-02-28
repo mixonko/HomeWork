ec56f34aefff44de02c32954bde569fd2607ff53
mixonko <m1zan1rop@mail.ru>
2019-02-28 13:42:07 +0300

    ChangeDzActivity

---
+++ app/src/main/java/dz2/ChangeDzActivity.java
@@ -0,0 +1,39 @@
+package dz2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import dz1.Dz1Activity;
+import test.com.homework.R;
+
+public class ChangeDzActivity extends Activity implements View.OnClickListener {
+    private Button dz1, dz2;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_dz);
+
+        dz1 = (Button) findViewById(R.id.dz1);
+        dz2 = (Button) findViewById(R.id.dz2);
+
+        dz1.setOnClickListener(this);
+        dz2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.dz1:
+                new Intent(this, Dz1Activity.class);
+                break;
+            case R.id.dz2:
+                new Intent(this, Dz2Activity.class);
+                break;
+        }
+    }
+}
