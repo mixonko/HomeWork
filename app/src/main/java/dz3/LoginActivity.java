c4d899d5af0ae9cb826e6db947a6ba2ef8baa72b
mixonko <m1zan1rop@mail.ru>
2019-02-28 21:53:06 +0300

    dz3 login

---
+++ app/src/main/java/dz3/LoginActivity.java
@@ -0,0 +1,15 @@
+package dz3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import test.com.homework.R;
+
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dz3_login);
+    }
+}
