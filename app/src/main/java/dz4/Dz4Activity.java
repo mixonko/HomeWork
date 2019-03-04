d113d7c17d27dffba92e6415c1cbf79d1619a061
mixonko <m1zan1rop@mail.ru>
2019-03-04 19:25:07 +0300

    Переход + сова

--- app/src/main/java/dz4/Dz4Activity.java
+++ app/src/main/java/dz4/Dz4Activity.java
@@ -1,15 +1,22 @@
 package dz4;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import test.com.homework.R;
 
 public class Dz4Activity extends Activity {
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dz4);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setBackgroundResource(R.drawable.animation_sova);
+        ((AnimationDrawable) imageView.getBackground()).start();
 
     }
 }
