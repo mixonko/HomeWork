cff3e3bbc145d9974e18a947336890effa91fa81
mixonko <m1zan1rop@mail.ru>
2019-03-04 23:51:38 +0300

    сова в часах

--- app/src/main/java/dz4/Clock.java
+++ app/src/main/java/dz4/Clock.java
@@ -34,13 +34,19 @@ public class Clock extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.save();
+        paint = new Paint();
+
+        drawClock(canvas, paint);
+        drawHours(canvas, paint);
+        drawMinutesSeconds(canvas, paint);
+    }
+
+    private void drawClock(Canvas canvas, Paint paint) {
         calendar = Calendar.getInstance();
         hours = calendar.get(Calendar.HOUR);
         minutes = calendar.get(Calendar.MINUTE);
         seconds = calendar.get(Calendar.SECOND);
 
-        paint = new Paint();
         paint.setStrokeWidth(5);
         paint.setTextSize(80);
         width = getWidth();
@@ -50,30 +56,26 @@ public class Clock extends View {
         canvas.drawCircle(width / 2, height / 2, 20, paint);
         paint.setStyle(Paint.Style.STROKE);
         canvas.drawCircle(width / 2, height / 2, radius, paint);
-
-        drawHours(canvas, paint);
-        drawMinutesSeconds(canvas, paint);
-        canvas.restore ();
-
     }
 
     private void drawHours(Canvas canvas, Paint paint) {
         paint.setStrokeWidth(10);
         for (int i = 1; i <= 12; i++) {
-            canvas.drawLine(width / 2, height / 2 - radius - min / 50, width / 2, height / 2 - radius + min / 50, paint);
+            canvas.drawLine(width / 2, height / 2 - radius - min / 50,
+                    width / 2, height / 2 - radius + min / 50, paint);
             canvas.rotate(-30, width / 2, height / 2);
 
         }
         int hoursText = 12;
         for (int i = 1; i <= 12; i++) {
-            canvas.drawText(String.valueOf(hoursText), width / 2 - 20, height / 2 - radius - min / 30, paint);
+            canvas.drawText(String.valueOf(hoursText), width / 2 - 20,
+                    height / 2 - radius - min / 30, paint);
             if (hours == hoursText) {
-                canvas.drawLine(width / 2, height / 2, width / 2, height / 2 - radius + 90, paint);
+                canvas.drawLine(width / 2, height / 2, width / 2,
+                        height / 2 - radius + 90, paint);
             }
             canvas.rotate(-30, width / 2, height / 2);
-
             hoursText--;
-
         }
     }
 
@@ -81,24 +83,21 @@ public class Clock extends View {
     private void drawMinutesSeconds(Canvas canvas, Paint paint) {
         paint.setStrokeWidth(3);
         for (int i = 1; i <= 60; i++) {
-            canvas.drawLine(width / 2, height / 2 - radius - min / 110, width / 2, height / 2 - radius + min / 110, paint);
+            canvas.drawLine(width / 2, height / 2 - radius - min / 110,
+                    width / 2, height / 2 - radius + min / 110, paint);
             canvas.rotate(6, width / 2, height / 2);
 
 
             if (minutes == i) {
                 paint.setStrokeWidth(7);
-                canvas.drawLine(width / 2, height / 2, width / 2, height / 2 - radius + 30, paint);
+                canvas.drawLine(width / 2, height / 2, width / 2,
+                        height / 2 - radius + 30, paint);
                 paint.setStrokeWidth(3);
             }
             if (seconds == i) {
-                canvas.drawLine(width / 2, height / 2, width / 2, height / 2 - radius, paint);
+                canvas.drawLine(width / 2, height / 2, width / 2,
+                        height / 2 - radius, paint);
             }
-
         }
-    }
-
-    private void currentTime() {
-
-
     }
 }
