59d59e6e5ee0bbc178104653cff0aa42cfd3f274
mixonko <m1zan1rop@mail.ru>
2019-03-26 18:42:40 +0300

    !

--- app/src/main/java/dz9/Dz9Activity.java
+++ app/src/main/java/dz9/Dz9Activity.java
@@ -1,6 +1,19 @@
 package dz9;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 
+import test.com.homework.R;
+
 public class Dz9Activity extends FragmentActivity {
+    Fragment fragment1, fragment2;
+    Boolean tablet = false;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+    }
 }
