a410c806de06fe5554d52aa4fcabdebcaa8cf175
mixonko <m1zan1rop@mail.ru>
2019-04-20 03:00:55 +0300

    dz 15 contact vodel

---
+++ app/src/main/java/dz15/Dz15Activity.java
@@ -0,0 +1,15 @@
+package dz15;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import test.com.homework.R;
+
+public class Dz15Activity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dz15_contact);
+    }
+}
