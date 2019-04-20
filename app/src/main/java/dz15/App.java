2c3729e93bc7956bdfa1c283d9ef20911cc3656c
mixonko <m1zan1rop@mail.ru>
2019-04-20 04:23:12 +0300

    get first contact

---
+++ app/src/main/java/dz15/App.java
@@ -0,0 +1,18 @@
+package dz15;
+
+import android.app.Application;
+import android.content.Context;
+
+public class App extends Application {
+
+    private static Context context;
+
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    public static Context getAppContext() {
+        return App.context;
+    }
+}
